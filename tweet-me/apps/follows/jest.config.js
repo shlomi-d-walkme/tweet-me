95c25a85d0a31d9915a3694ebfc167cfd3153a0f
Adar Kaisar <adark3@gmail.com>
2021-10-11 18:02:03 +0300

    Added follows nest + upgrading package json

---
+++ tweet-me/apps/follows/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'follows',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/follows',
+};
