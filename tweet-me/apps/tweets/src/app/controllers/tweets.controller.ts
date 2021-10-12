464c54c51b15c8ed523dce04281f13270acbc1cb
Amir <amir@example.com>
2021-10-12 22:14:25 +0300

    adding all methods

--- tweet-me/apps/tweets/src/app/controllers/tweets.controller.ts
+++ tweet-me/apps/tweets/src/app/controllers/tweets.controller.ts
@@ -1,24 +1,35 @@
 import { Controller } from '@nestjs/common';
-import { Body, Delete, Get, Post } from '@nestjs/common';
+import { Delete, Get, Post, Put, Param } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
 import { TweetsDto } from '../dto/tweets-dto';
 
 @Controller('tweets')
 export class Tweets {
-    @Get()
-    @ApiOkResponse({type: [TweetsDto]})
-    getTweets(): TweetsDto[] {
+    @Get('/get/:profileId/')
+    @ApiOkResponse({ status: 200, type: [TweetsDto], description: 'Returns tweets by user ids'})
+    getTweets(@Param('profileId') profileId: string): TweetsDto[] {
+        console.log("getTweets", profileId);
         return [new TweetsDto()];
     }
 
-    @Post()
-    @ApiOkResponse({type: TweetsDto})
-    addTweet() {
+    @Post('/add/:profileId/')
+    @ApiOkResponse({ status: 201, type: TweetsDto, description: 'Add a tweet' })
+    addTweet(@Param('profileId') profileId: string, @Param('content') content: string) {
+        console.log("addTweet", profileId, content);
         return undefined;
     }
 
-    @Delete()
-    deleteTweet() {
+    @Delete('delete/:tweetId/')
+    @ApiOkResponse({ status: 204, type: Boolean, description: 'Remove a tweet' })
+    deleteTweet(@Param('tweetId') tweetId: string) {
+        console.log("deleteTweet", tweetId)
+        return undefined;
+    }
+
+    @Put('update/:tweetId/')
+    @ApiOkResponse({ status: 204, type: Boolean, description: 'Remove a tweet' })
+    updateTweet(@Param('tweetId') tweetId: string, @Param('content') content: string) {
+        console.log("updateTweet", tweetId, content)
         return undefined;
     }
 
