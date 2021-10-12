4c8ae1fad15093edc908968240faf0d25e7f22a3
Amir <amir@example.com>
2021-10-12 09:33:59 +0300

    change port

--- tweet-me/apps/tweets/src/main.ts
+++ tweet-me/apps/tweets/src/main.ts
@@ -12,10 +12,19 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
+  const port = process.env.PORT || 4444;
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
   });
 }
 
 bootstrap();
+
+// API:
+// Tweet
+// Post
+// Delete
+// Update
+// Retweet
+// Reply
+// TweetsByUser
