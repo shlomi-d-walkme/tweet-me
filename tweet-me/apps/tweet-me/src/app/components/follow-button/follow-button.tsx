6a8a454636b215a848dddbf3060c714aa0db510b
Adar Kaisar <adark3@gmail.com>
2021-10-13 13:39:44 +0300

    Add follow button componant draft

---
+++ tweet-me/apps/tweet-me/src/app/components/follow-button/follow-button.tsx
@@ -0,0 +1,14 @@
+import './follow-button.module.scss';
+
+/* eslint-disable-next-line */
+export interface FollowButtonProps {}
+
+export function FollowButton(props: FollowButtonProps) {
+  return (
+    <div>
+      <h1>Welcome to FollowButton!</h1>
+    </div>
+  );
+}
+
+export default FollowButton;
