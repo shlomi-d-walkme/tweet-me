38cfeca8b6ccba59ac469e26667b3fa69d8d7fcb
Michael Lando <miclando@gmail.com>
2021-10-13 10:27:56 +0300

    tests

--- tweet-me/apps/profile/src/app/app.controller.spec.ts
+++ tweet-me/apps/profile/src/app/app.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProfileDto } from './dto/profile.dts';
+import { Profile } from './entities/profile.entity';
 
 describe('AppController', () => {
   let app: TestingModule;
@@ -13,12 +15,37 @@ describe('AppController', () => {
     }).compile();
   });
 
-  describe('getData', () => {
-    it('should return "Welcome to profile!"', () => {
+  describe('register', () => {
+    it('a new profile is creatrd', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({
-        message: 'Welcome to profile!',
-      });
+      const profileDts= new ProfileDto('test@email.com','bob','smith','pass','bob');
+
+      appController.register(profileDts);
+
+      expect(appController.getProfiles().length).toEqual(1);
+      expect(appController.getProfile("")).toEqual(Profile.convertToEntity(profileDts));
+    });
+  });
+
+  describe('profiles', () => {
+    it('get profiles', () => {
+      const appController = app.get<AppController>(AppController);
+      const profileDts= new ProfileDto('test@email.com','bob','smith','pass','bob');
+      appController.register(profileDts);
+      const profileDts1= new ProfileDto('test1@email.com','bob1','smith1','pass1','bob1');
+      appController.register(profileDts1)
+      expect(appController.getProfiles().length).toEqual(2);
+    });
+  });
+
+  describe('profile', () => {
+    it('get profile', () => {
+      const appController = app.get<AppController>(AppController);
+      const profileDts= new ProfileDto('test@email.com','bob','smith','pass','bob');
+      const res=appController.register(profileDts);
+      const profileDts1= new ProfileDto('test1@email.com','bob1','smith1','pass1','bob1');
+      const res1=appController.register(profileDts1)
+      expect(appController.getProfiles().length).toEqual(2);
     });
   });
 });
--- tweet-me/apps/profile/src/app/dto/profile.dts.ts
+++ tweet-me/apps/profile/src/app/dto/profile.dts.ts
@@ -1,6 +1,15 @@
 import { IsDateString, IsInt, IsString } from 'class-validator';
 
 export class ProfileDto {
+
+  constructor(email: string,firstName: string,lastName: string,passward: string,userName: string) {
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.passward = passward;
+    this.userName = userName;
+  }
+  
   @IsString()
   readonly userName: string;
 
