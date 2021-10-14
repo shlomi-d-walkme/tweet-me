7295aa0d10834ab07b73adf5f6d02d2281c7a8f8
Adar Kaisar <adark3@gmail.com>
2021-10-14 12:41:11 +0300

    we are first!!!!

--- tweet-me/apps/gq-gateway/src/app/app.module.ts
+++ tweet-me/apps/gq-gateway/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
+
 })
 export class AppModule {}
