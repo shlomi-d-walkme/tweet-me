a9ef8d25a83f42d93a0251e2694aa4b3b6f55078
Adar Kaisar <adark3@gmail.com>
2021-10-14 16:54:40 +0300

    Updated follows sdk and gq

--- apps/gq-gateway/src/app/follows/follows.resolver.ts
+++ apps/gq-gateway/src/app/follows/follows.resolver.ts
@@ -2,32 +2,31 @@ import { Args, Parent, Query, Resolver, ResolveField } from "@nestjs/graphql";
 import { Follow } from "./follows.model";
 import fetch from 'cross-fetch';
 
+import { Configuration, DefaultApi } from '@tweet-me/sdk/follows-sdk'
+
+const api = new DefaultApi(new Configuration({basePath: 'http://localhost:5555'}))
+
 @Resolver(of => Follow)
 export class FollowsResolver {
 
     @ResolveField()
-    followers(@Parent() follow) {
+    async followers(@Parent() follow:Follow) {
         const { profileId } = follow
+        const result = await api.followsControllerGetFollowersByUser(profileId);
+        return result.data.followers;
+    }
 
-        const requestOptions = {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' }
-        };
-
-        fetch(`http://localhost:5555/api/follows/${profileId}/followers`, requestOptions)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    return result.followers
-                },
-                (error) => {
-                    console.log("ERROR" + error);
-                }
-            );
+    @ResolveField()
+    async following(@Parent() follow:Follow) {
+        const { profileId } = follow
+        const result = await api.followsControllerGetFollowingByUser(profileId);
+        return result.data.following;
     }
 
     @Query(() => Follow)
     async getFollowInfo(@Args('profileId', { type: () => String }) profileId: string) {
         return { profileId }
     }
+
+    
 }
