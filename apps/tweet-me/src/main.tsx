4a5e5faf7f3de40e6dfc07074c25c4784bac4c2f
BootCamp-2 <bootcamp-2@ladmins-Air.lan>
2021-10-15 18:47:39 +0300

    connect apollo to app

--- apps/tweet-me/src/main.tsx
+++ apps/tweet-me/src/main.tsx
@@ -1,11 +1,25 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom';
+import { StrictMode } from "react";
+import * as ReactDOM from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from "@apollo/client";
+import App from "./app/app";
 
-import App from './app/app';
+const client = new ApolloClient({
+  uri: "http://localhost:5333/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
+  <ApolloProvider client={client}>
+    <StrictMode>
+      <App />
+    </StrictMode>{" "}
+  </ApolloProvider>,
+
+  document.getElementById("root")
 );
