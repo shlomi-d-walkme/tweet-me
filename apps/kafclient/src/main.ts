2d72d1833f49fe38476a0de6a180154afb4827ff
BootCamp-3 <bootcamp-3@h-MacBook-Air-sl-ladmin.local>
2021-10-24 15:49:04 +0300

    kafka main client

--- apps/kafclient/src/main.ts
+++ apps/kafclient/src/main.ts
@@ -1,1 +1,19 @@
+import { Kafka, logLevel } from 'kafkajs'
+
 console.log('Hello World!');
+
+const main = async() => {
+    const kafka = new Kafka({ logLevel: logLevel.ERROR, brokers: ["localhost:9092"]});
+    const consumer = kafka.consumer({ groupId: 'my-microservice-group2'});
+    await consumer.connect();
+    await consumer.subscribe({ topic: 'tweets', fromBeginning: true });
+    await consumer.run({
+        autoCommit: true,
+        eachMessage: async (payload) => {
+            const messageBody = payload.message.value.toString();
+            console.log(messageBody);
+        } 
+    })
+}
+
+main().then();
