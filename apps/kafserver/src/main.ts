a573ecbd28abb317080c86d2857bea710284c6fc
BootCamp-3 <bootcamp-3@h-MacBook-Air-sl-ladmin.local>
2021-10-24 15:32:11 +0300

    kafka main

--- apps/kafserver/src/main.ts
+++ apps/kafserver/src/main.ts
@@ -1,1 +1,20 @@
+import { Kafka, Partitioners } from 'kafkajs'
+
 console.log('Hello World!');
+const main = async () => {
+    const kafka = new Kafka({ brokers: ["localhost:9092"]});
+    const producer = kafka.producer({
+        createPartitioner: Partitioners.JavaCompatiblePartitioner
+    });
+
+    await producer.connect();
+    await producer.send({
+        topic: 'tweets',
+        messages: [
+            { key: 'user1', value: 'Whats upppp' },
+            { key: 'user2', value: 'All goooooodddd' }
+        ]
+    })
+};
+
+main().then();
