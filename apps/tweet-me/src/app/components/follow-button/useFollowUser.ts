e128d943dccbf611e4fa659198969ce240aeb9d7
Adar Kaisar <adark3@gmail.com>
2021-10-26 14:15:01 +0300

    Follow button works

---
+++ apps/tweet-me/src/app/components/follow-button/useFollowUser.ts
@@ -0,0 +1,33 @@
+import { gql, useMutation } from "@apollo/client";
+
+export interface FollowUserVars {
+    profileId: string,
+    followProfileId: string
+  }
+  
+export interface FollowUser {
+    followUser: boolean
+}
+    
+const FOLLOW_USER = gql`
+mutation FollowUser($profileId: String!, $followProfileId: String!) {
+followUser(profileId: $profileId, followProfileId: $followProfileId)
+}
+`;
+    
+export function useFollowUser() {
+    const [followUser, { data, loading, error }] = useMutation<FollowUser, FollowUserVars>(FOLLOW_USER);
+
+    const follow = (profileId: string, followProfileId: string) => {
+        followUser({
+        variables: {
+            profileId,
+            followProfileId
+        }
+        });
+    }
+
+    return {
+        follow, data, loading, error
+    }
+}
---
+++ apps/tweet-me/src/app/components/follow-button/useUnfollowUser.ts
@@ -0,0 +1,33 @@
+import { gql, useMutation } from "@apollo/client";
+
+export interface UnfollowUserVars {
+    profileId: string,
+    unfollowProfileId: string
+  }
+  
+export interface UnfollowUser {
+    unfollowUser: boolean
+}
+    
+const UNFOLLOW_USER = gql`
+mutation FollowUser($profileId: String!, $unfollowProfileId: String!) {
+unfollowUser(profileId: $profileId, unfollowProfileId: $unfollowProfileId)
+}
+`;
+    
+export function useUnfollowUser() {
+    const [unfollowUser, { data, loading, error }] = useMutation<UnfollowUser, UnfollowUserVars>(UNFOLLOW_USER);
+
+    const unfollow = (profileId: string, unfollowProfileId: string) => {
+        unfollowUser({
+        variables: {
+            profileId,
+            unfollowProfileId
+        }
+        });
+    }
+
+    return {
+        unfollow, data, loading, error
+    }
+}
