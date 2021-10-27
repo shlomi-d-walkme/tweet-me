b58b5ee3910c8d7f738126878c6425fe0c1f3624
BootCamp-2 <bootcamp-2@ladmins-MacBook-Air.local>
2021-10-27 09:13:23 +0300

    new tweet

---
+++ apps/tweet-me/src/app/newTweet/newTweet.spec.tsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+
+import Tweet from './tweet.component';
+
+describe('Tweet', () => {
+  it('should render successfully', () => {
+    const { baseElement } = render(<Tweet/>);
+    expect(baseElement).toBeTruthy();
+  });
+});
