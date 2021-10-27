7e8d9368df248a43638d1eff77ba0e09c85fbf85
Adar Kaisar <adark3@gmail.com>
2021-10-27 12:58:44 +0300

    update uI

--- apps/tweet-me/src/app/components/follow-button/follow-button.tsx
+++ apps/tweet-me/src/app/components/follow-button/follow-button.tsx
@@ -1,18 +1,14 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import css from './follow-button.module.scss';
-import { useMutation, gql, useQuery } from "@apollo/client";
-import { useFollowUser, FollowUser } from './useFollowUser';
+import { useFollowUser } from './useFollowUser';
 import { useUnfollowUser } from './useUnfollowUser';
 
 export interface FollowButtonProps {
-  isFollowing: boolean
+  isFollowing: boolean,
+  profileId?: string
 }
 
-export interface FollowsInput {
-  profileId: string
-}
-
-export function FollowButton({ isFollowing }: FollowButtonProps, {profileId}: FollowsInput) {
+export function FollowButton({ isFollowing, profileId }: FollowButtonProps) {
   const [isFollowingState, setIsFollowing] = useState(isFollowing);
   const { follow, loading: followingLoading } = useFollowUser();
   const { unfollow, loading: unfollowingLoading } = useUnfollowUser();
@@ -34,5 +30,3 @@ export function FollowButton({ isFollowing }: FollowButtonProps, {profileId}: Fo
     </div>
   );
 }
-
-export default FollowButton;
