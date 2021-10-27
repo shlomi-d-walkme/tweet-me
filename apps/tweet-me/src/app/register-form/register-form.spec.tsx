915ac119f6fe34e72b94240824105a4e9c8f841d
Adar Kaisar <adark3@gmail.com>
2021-10-27 12:32:41 +0300

    update register button

--- apps/tweet-me/src/app/register-form/register-form.spec.tsx
+++ apps/tweet-me/src/app/register-form/register-form.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import RegisterForm from './register-form';
+import RegisterForm from './register-form.component';
 
 describe('RegisterForm', () => {
   it('should render successfully', () => {
