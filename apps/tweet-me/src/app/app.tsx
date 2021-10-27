c07a14c7a43207077740e2d4a68a2628c957d87c
Adar Kaisar <adark3@gmail.com>
2021-10-27 16:41:06 +0300

    remove button

--- apps/tweet-me/src/app/app.tsx
+++ apps/tweet-me/src/app/app.tsx
@@ -36,7 +36,6 @@ export const App = () => {
 
         <Route path="/register">
           <RegisterForm></RegisterForm>
-          <SignUpButton />
         </Route>
 
         <Route path="/feed">
